1ae99201dd61fce61d3bbe5115310e8b615b237b
Kajol sutra dhar <kajol.sd777@gmail.com>
2024-12-19 01:15:36 +0200

    Code cleaned: Error page, files

--- src/components/common/Error.jsx
+++ src/components/common/Error.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import PropTypes from "prop-types";
 import { Box, Typography } from "@mui/material";
 
 const Error = ({ message }) => (
@@ -8,6 +8,10 @@ const Error = ({ message }) => (
       justifyContent: "center",
       alignItems: "center",
       height: "100vh",
+      backgroundColor: "#f8d7da",
+      padding: "20px",
+      boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+      borderRadius: "8px",
     }}
   >
     <Typography variant="h6" color="error">
@@ -15,5 +19,9 @@ const Error = ({ message }) => (
     </Typography>
   </Box>
 );
+
+Error.propTypes = {
+  message: PropTypes.string.isRequired,
+};
 
 export default Error;
