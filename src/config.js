00c503875cded97cf4526211ed72e5d1982a0501
Kajol sutra dhar <kajol.sd777@gmail.com>
2024-12-19 02:07:29 +0200

    Code cleaned: Refactore: config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
-export const baseUrl = import.meta.env.VITE_DRUPAL_HOST_URL || "https://druid-backend.lndo.site";
-export const mauticBaseUrl = import.meta.env.VITE_MAUTIC_HOST_URL || "https://mautic-lando.lndo.site";
-export const drupalBaseUrl = "https://druid-backend.lndo.site";
-export const mauticContactsApiUrl = `${drupalBaseUrl}/api/mautic-contacts`; // Centralized URL
+const defaultDrupalUrl = "https://druid-backend.lndo.site";
+const defaultMauticUrl = "https://mautic-lando.lndo.site";
+
+export const baseUrl = import.meta.env.VITE_DRUPAL_HOST_URL || defaultDrupalUrl;
+export const mauticBaseUrl = import.meta.env.VITE_MAUTIC_HOST_URL || defaultMauticUrl;
+export const drupalBaseUrl = defaultDrupalUrl;
+export const mauticContactsApiUrl = `${drupalBaseUrl}/api/mautic-contacts`; 
